a478c00436eab7b16beacbb8001600d81afb9df3
sweetkeys <shaun@shaunsweet.com>
2016-08-29 23:41:26 -0700

    merge

--- jarvis.rb
+++ jarvis.rb
@@ -4,8 +4,8 @@ require_relative "./environment.rb"
 
 bot = Discordrb::Bot.new token: ENV["discord_token"], application_id: ENV["discord_app_id"].to_i
 now = Date.today
-bot.message(with_text: 'job') do |event|
-	todays_jobs = Job.where("date_posted = ?", now)
+bot.message with_text: 'job' do |event|
+	todays_jobs = Job.where "date_posted = ?", now
 	response = "Greetings sir... I have found a few jobs for you...\n"
 	if todays_jobs.length > 0
 		todays_jobs.each do |job|
@@ -21,3 +21,4 @@ bot.message(with_text: 'job') do |event|
 end
 
 bot.run
+
