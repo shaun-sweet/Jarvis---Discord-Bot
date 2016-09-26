11eac0500fed31821202fd302f11753b689d231b
sweetkeys <shaun@shaunsweet.com>
2016-09-25 19:36:54 -0700

    test

--- jarvis.rb
+++ jarvis.rb
@@ -1,10 +1,15 @@
 require_relative "./environment.rb"
 
-bot = Discordrb::Commands::CommandBot.new token: ENV["discord_token"], application_id: ENV["discord_app_id"].to_i, prefix: 'jarvis '
+bot = Discordrb::Commands::CommandBot.new token: ENV["discord_token"], application_id: ENV["discord_app_id"].to_i, prefix: '/'
 
 
 bot.command :dice do |event, min, max|
+
 	rand(min.to_i .. max.to_i)
+end
+
+bot.command :role_me do |event, role|
+	"your userid is: #{event.user.id}"
 end
 
 bot.run
